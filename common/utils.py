2d689b735308f6402ec68d53d53a5d82cc5b753d
sodagayethiam <sodagayethiam@gmail.com>
2024-09-18 16:48:46 +0000

    modification du dataframe de pyspark à pandas, ajout de fonctions de traduction et de nettoyage de texte dans utils, process du df companeies

--- common/utils.py
+++ common/utils.py
@@ -1,21 +1,24 @@
 """
 Toutes les fonctions utilisées dans le projet sont définies dans ce fichier (read, write, get,..)
 """
+import pandas as pd
 
 import sys
 sys.path.append('../')
 
-from pyspark.sql import DataFrame
+#from pyspark.sql import DataFrame
 
 
 from configs.conf import es
 from elasticsearch.helpers import bulk
 
-
+import re
+from langdetect import detect
+from deep_translator import GoogleTranslator
 
 
 #Write DF in Elasticsearch
-def generate_data_to_elk(df: DataFrame):
+def generate_data_to_elk(df: pd.DataFrame):
     for index, row in df.iterrows():
         yield {
             "_index": "bigdata-adzuna", 
@@ -24,7 +27,7 @@ def generate_data_to_elk(df: DataFrame):
         }
 
 
-def insert_data_elk(df: DataFrame):
+def insert_data_elk(df: pd.DataFrame):
     #Insert DataFrame in Elasticsearch
     try:
         # Convert DataFrame and insert in Elasticsearch
@@ -38,3 +41,33 @@ def insert_data_elk(df: DataFrame):
 def extract_values(column_to_extract, value):
     values_to_extract = [item[value] for item in column_to_extract]
     return values_to_extract
+
+#Fonction qui traduit un texte du francaias à l'angalis 
+def translate_french_to_english(text):
+    if pd.notna(text) and isinstance(text, str) and len(text.strip()) > 1:  # Vérifiez si le texte est significatif
+        try:
+            lang = detect(text)  
+            if lang == 'fr':
+                translator = GoogleTranslator(source='fr', target='en')
+                translation = translator.translate(text)
+                return translation
+            else:
+                return text  
+        except Exception as e:
+            print(f"Erreur de traduction ou de détection : {e}")
+            return text  
+    return text
+
+
+#Fonction pour nettoyer un texte
+    
+def clean_text(text):
+    if pd.notna(text) and isinstance(text, str):
+        # Remplacer les caractères spéciaux Unicode par des espaces 
+        text = text.replace('\u202f', ' ')
+        #retours à la ligne
+        text = text.replace('\n', ' ')
+        # Supprimer les espaces multiples
+        text = re.sub(r'\s+', ' ', text)
+        return text
+    return ""
--- webscraping/process_companies.py
+++ webscraping/process_companies.py
@@ -1,27 +1,84 @@
+
+#export PYTHONPATH=$(pwd)
+
 #import packages
 import pickle
 import pandas as pd
+from tqdm import tqdm
+tqdm.pandas()  # Initialiser tqdm pour pandas
+from common.utils import translate_french_to_english
+from common.utils import clean_text
+#import re
+#from langdetect import detect
+#from deep_translator import GoogleTranslator
+
 
 #affichage des champs 
 pd.set_option('display.max_colwidth', None)
 
-#chargement du df des compagnies
+#chargement du df des compagnies welcome to the jungle
 companies_wttj = pd.read_pickle('data/raw/companies_wttj_aout.pkl')
-companies_wttj.describe()
+companies_themuse = pd.read_pickle('/Users/sodagayethiam/Documents/Formations/Parcours_data_engineer/projet_jobmarket/JobMarket/data/raw/companies_muse_sept.pkl')
 
+
+
+#traitement des noms de compagnies manquantes da
 companies_wttj['name_company2']=companies_wttj['url_company'].str.extract(r'companies/([a-zA-Z0-9-]+)')
 companies_wttj.loc[companies_wttj['name_company'].isnull(), 'name_company'] =companies_wttj['name_company2']
 companies_wttj.drop('name_company2', axis=1, inplace=True)
 
 #ajout de la source
 companies_wttj['source']='wttj'
+companies_themuse['source']='TheMuse'
 
-#transformer la liste en string
-companies_wttj['contents'] = companies_wttj['contents'].apply(' '.join)
+#Nettoyer les champs des descriptions , locations et industriers
+companies_wttj['contents'] = companies_wttj['contents'].astype(str)
+# Supprimer les crochets '[ ]'
+companies_wttj['contents'] = companies_wttj['contents'].str.replace(r'[\[\]]', '', regex=True)
 
+companies_themuse['locations'] = companies_themuse['locations'].apply(' '.join)
+companies_themuse['industries'] = companies_themuse['industries'].apply(' '.join)
+
+
+#CSelectionned columns
+themuse_selected = companies_themuse[['name', 'twitter','description','refs.landing_page','publication_date',
+'size.name', 'refs.logo_image','locations','industries', 'refs.jobs_page' ,'source']].rename(columns={'name': 'name_company', 'twitter': 'twitter_link',
+'refs.landing_page': 'company_website','size.name': 'size','refs.logo_image': 'logo','refs.jobs_page': 'url_jobs'})
+
+themuse_selected['linkedin_link']='not available'
+themuse_selected['facebook_link']='not available'
+themuse_selected['instagram_link']='not available'
+themuse_selected['creation_date']='not available'
+themuse_selected['nb_employee']='not available'
+themuse_selected['parity_women']='not available'
+themuse_selected['parity_men']='not available'
+themuse_selected['avg_age_employees']='not available'
+themuse_selected['ca']='not available'
+
+
+wttj_selected = companies_wttj[['name_company', 'linkedin_link', 'twitter_link_element','facebook_link','instagram_link','contents',
+'company_website','creation_date', 'nb_employee','location' ,'domain', 'url_company','parity_women',
+'parity_men','avg_age','ca','source']].rename(columns={'twitter_link_element': 'twitter_link', 'contents': 'description',
+'location': 'locations','domain': 'industries','url_company': 'url_jobs','avg_age': 'avg_age_employees'})
+
+wttj_selected['publication_date']='not available'
+wttj_selected['size']='not available'
+wttj_selected['logo']='not available'
+
+companies = pd.concat([themuse_selected, wttj_selected], axis=0)
+shape=companies.shape
+print(f"Taille du dataframe fusionné : {shape}")
+
+
+
+#traduction des textes en Francais 
+companies['contents'] = companies['description'].apply(clean_text)
+companies['contents'] = companies['contents'].progress_apply(translate_french_to_english)
+
+#save the result
+
+companies.to_pickle('./data/final/companies.pkl')
 
 #companies_wttj['creation_date'] = companies_wttj['creation_date'].astype(int)
 #companies_wttj['nb_employee'] = companies_wttj['nb_employee'].astype(int)
 
-print(companies_wttj.head(2))
-print(companies_wttj.dtypes)
