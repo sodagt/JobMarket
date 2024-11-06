30e1e3b4cdb23e50a6a66770be29dfc04819f383
AMINATA NDAO <kaminandao@gmail.com>
2024-11-06 21:26:47 +0000

    integrate selenium

--- webscraping/linkedin.py
+++ webscraping/linkedin.py
@@ -5,6 +5,7 @@ Cette classe nous permet de webscrapper les données du site LinkedIn.
 
 #import sys
 #sys.path.append('../')
+import os
 
 import pandas as pd
 import pickle
@@ -172,33 +173,167 @@ for job_link in job_links:
 
 
 
-company_name,company_description,company_website,company_industries,company_size,company_location,company_type,company_creation_date,company_specialities = [],[],[],[],[],[],[],[],[]
+company_name,company_description,company_website,company_industries,company_size,company_associated_members,company_location,company_type,company_creation_date,company_specialities = [],[],[],[],[],[],[],[],[],[]
 
 
 #get more information from company link
 
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Replace with your LinkedIn credentials
+username = os.getenv("$LINKEDIN_USERNAME")
+password = os.getenv("$LINKEDIN_PASSWORD")
+print(username)
+print(password)
+
+# Setup Chrome driver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+# Open LinkedIn login page
+driver.get("https://www.linkedin.com/login")
+
+# Find the username and password fields and fill them in
+driver.find_element(By.ID, "username").send_keys("oadnanimak@gmail.com")
+driver.find_element(By.ID, "password").send_keys("ProjetFormationDE#2024")
+
+# Submit the login form
+driver.find_element(By.XPATH, "//button[@type='submit']").click()
+
+# Wait for login to complete (adjust as necessary)
+time.sleep(15)
+
 for company_link in company_links:
         
         #linkedin_company_link = requests.get(company_link)
-        url_company='https://www.linkedin.com/company/computercore?trk=public_jobs_topcard-org-name'
-        linkedin_company_link = requests.get(url_company)
-        soup_company_link = bs_linkedin(linkedin_company_link.content,"lxml")
+        ##url_company='https://www.linkedin.com/company/computercore?trk=public_jobs_topcard-org-name'
+        ##linkedin_company_link = requests.get(url_company)
+        ##soup_company_link = bs_linkedin(linkedin_company_link.content,"lxml")
         #company_name, company_website, description, publication_date, creation_date, nb_employee, size
         #logo, location, industries, parity_women parity_men, avg_age_employees, ca, source, insta/face
 
+        ##linkedin_company_link = requests.get(company_link['href'])
+        ##soup_company_link = bs_linkedin(linkedin_company_link.content,"lxml")
+        print("/////// Linkedin Companies LInks /////// ")
+        ##print( soup_company_link)
+        #name = soup_company_link.find('h1',class_='top-card-layout__title')
+        #print("company link, name :" + name)
+
+        print(company_link)
+        #parts = company_link.split('?')
+        #print(parts)
+        #company_link_about = ''.join(parts[0]) + '/about/'
+        company_link_about = company_link.split('?')[0] + '/about/'
+
+        print(company_link_about)
+
+        # Navigate to the company page (replace with the URL of the company you want)
+        #company_url = company_link['href']
+        driver.get(company_link_about)
+        soup_company_link = bs_linkedin(driver.page_source,'html.parser')
+
+
+
+        # Wait for the page to load (adjust time as necessary)
+        time.sleep(5)
+
+        # Extract the company name (this may need to be adjusted depending on the page structure)
+        try:
+            print(driver.current_url)
+            from selenium.webdriver.support.ui import WebDriverWait
+            from selenium.webdriver.support import expected_conditions as EC
+
+            # Wait for the company name (h1 tag) to be visible
+            time.sleep(5)
+            name = soup_company_link.find('h1',class_='ember-view')['title']
+            print(name)
+            company_name.append(name)
+             #WebDriverWait(driver, 10).until(
+                #EC.visibility_of_element_located((By.TAG_NAME, 'h1'))
+            #)
+            
+            
+            description = soup_company_link.find('p',class_='break-words')
+            if description is not None:
+                company_description.append(description.get_text(strip=True))
+                print(description.text.strip())
+
+            website_scrap = soup_company_link.find('h3',class_='text-heading-medium')
+            if (website_scrap.text.strip() == 'Website') : 
+                website = website_scrap.find('span',class_='link-without-visited-state')
+                if website is not None:
+                    company_website.append(website.get_text(strip=True))
+                    print(website.get_text(strip=True))
+
+            industries_scrap = soup_company_link.find('h3',class_='text-heading-medium')
+            if (industries_scrap.text.strip() == 'Industry') : 
+                if soup_company_link.find('dd',class_='mb4') is not None:
+                    industries = industries_scrap.find('dd',class_='mb4').get_text(strip=True)
+                    company_industries.append(industries)
+                    print(industries)
+
+            size_scrap = soup_company_link.find('h3',class_='text-heading-medium')
+            if (size_scrap.text.strip() == 'Company size') : 
+                if soup_company_link.find('dd',class_='t-black--light') is not None:
+                    size = size_scrap.find('dd',class_='t-black--light').text.strip()
+                    company_size.append(size)
+                    print(size)
+
+            
+            associated_members=soup_company_link.find('dd',class_='t-black--light').find('a',class_='ember-view').find("span")
+            if associated_members is not None:
+                company_associated_members.append(associated_members.text.strip() )
+                print(associated_members)
+                
+            
+            location_scrap = soup_company_link.find('h3',class_='text-heading-medium')
+            if (location_scrap.text.strip() == 'Headquarters') : 
+                if location_scrap.find('dd',class_='mb4') is not None:
+                    location = location_scrap.find('dd',class_='mb4').text.strip()
+                    company_location.append(location)
+                    print(location)
+
+            creation_date_scrap = soup_company_link.find('h3',class_='text-heading-medium')
+            if (creation_date_scrap.text.strip() == 'Founded') : 
+                if creation_date_scrap.find('dd',class_='mb4') is not None:
+                    creation_date = creation_date_scrap.find('dd',class_='mb4').text.strip()
+                    company_creation_date.append(creation_date)
+                    print(creation_date)
+
+            specialities_scrap = soup_company_link.find('h3',class_='text-heading-medium')
+            if (specialities_scrap.text.strip() == 'Specialities') : 
+                if specialities_scrap.find('dd',class_='mb4') is not None:
+                    specialities = specialities_scrap.find('dd',class_='mb4').text.strip()
+                    company_specialities.append(specialities)
+                    print(specialities)
+            
+            ##name = driver.find_element(By.TAG_NAME, 'h1').text
+            ##print(f"Company Name: {name}")
+        except Exception as e:
+            print(f"Error extracting company name: {e}")
+
+        # Close the browser window after use
 
         #get company info
 
         #name_test = soup_company_link.find('h1',class_='top-card-layout__title')
         #if name_test is not None:
-        print( soup_company_link)
-        name = soup_company_link.find('h1',class_='top-card-layout__title')
-        print("company link, name :" + name)
+        
+        ##print( soup_company_link)
+        ##name = soup_company_link.find('h1',class_='top-card-layout__title')
+        ##print("company link, name :" + name)
+
         #.text.strip()
-        '''
-        company_name.append(name)
+        
+        
+        time.sleep(10)
 
-
+'''
        # description_test = soup_company_link.find('p',class_='break-words')
        # if description_test is not None:
         description = soup_company_link.find('p',class_='break-words').text.strip()
@@ -245,6 +380,7 @@ for company_link in company_links:
 
 
 
+driver.quit()
     
 
 
@@ -269,9 +405,9 @@ df_linkedin_scrapping['ingest_date'] = pd.Timestamp.now()
 df_linkedin_scrapping.shape
 
 
-liste_company = [company_name,company_description,company_website,company_industries,company_size,company_location,company_type,company_creation_date,company_specialities]
-df_linkedin_company_scrapping = pd.DataFrame(list(zip(company_name,company_description,company_website,company_industries,company_size,company_location,company_type,company_creation_date,company_specialities)),
-                                          columns=["company_name","company_description","company_website","company_industries","company_size","company_location","company_type","company_creation_date","company_specialities"]).reset_index(drop=True)
+liste_company = [company_name,company_description,company_website,company_industries,company_size,company_associated_members,company_location,company_type,company_creation_date,company_specialities]
+df_linkedin_company_scrapping = pd.DataFrame(list(zip(company_name,company_description,company_website,company_industries,company_size,company_associated_members,company_location,company_type,company_creation_date,company_specialities)),
+                                          columns=["company_name","company_description","company_website","company_industries","company_size","company_associated_members","company_location","company_type","company_creation_date","company_specialities"]).reset_index(drop=True)
 df_linkedin_company_scrapping["source"] = "linkedin"
 df_linkedin_company_scrapping['ingest_date'] = pd.Timestamp.now()
 
