ca824e59aaa4c2a583da9ba4bad88cffc8c88073
AMINATA NDAO <kaminandao@gmail.com>
2024-11-23 18:21:10 +0000

    api query from db source elastic

--- flask/api.py
+++ flask/api.py
@@ -13,7 +13,7 @@ import pprint
 api = Flask(__name__)
 
 
-jobs = pd.read_pickle('outputs/final/jobs.pkl')
+jobs = pd.read_pickle('../outputs/final/jobs.pkl')
 
 jobs_full = jobs[['job_title', 'contract_time', 'company_name', 'salary_min', 'salary_max', 'publication_datetime', 'location', 'industry', 'offer_link', 'source', 'levels', 'category', 'contents', 'publication_time', 'publication_date', 'contract_type', 'country', 'state', 'city', 'postcode', 'latitude', 'longitude', 'currency_name', 'job_description', 'type_contract', 'addressCountry']]
 
@@ -25,6 +25,21 @@ def handler_error404(err):
   return "You have encountered an error of 404",404
 
 api.register_error_handler(404,handler_error404)
+
+
+
+from elasticsearch import Elasticsearch
+
+
+es = Elasticsearch(
+    [{'host': 'localhost', 'port': 9200, 'scheme': 'https'}],
+    http_auth=('elastic', 'datascientest'), 
+    timeout=600,
+    verify_certs=True,  # Check SSL certificats
+    ca_certs='../elasticsearch/ca/ca.crt'  # Specify CA certificat path
+
+)
+
 
 
 class Country(BaseModel):
@@ -68,7 +83,7 @@ def country(query:Country):
 
     return jobs_country_json
 
-    """
+    
 
 @api.route("/jobs",methods=["POST","GET"])
 def jobs():
@@ -77,29 +92,73 @@ def jobs():
         return "Hello {} \n".format(data["name"])
     return jobs_full_json
 
+    """
+
+@api.route("/jobs",methods=["POST","GET"])
+def jobs():
+    result = es.search(
+        size=1000,
+        index='bigdata-jobs',
+        query={
+        'match_all': {}
+         }
+    )
+
+    final_result = result['hits']['hits']
+    return final_result
+
+#http://127.0.0.1:2222/jobs
+
 
 @api.route("/jobs/country")
 @validate()
 def country(query:Country):
     country_name = query.country
-    jobs_country = jobs_full[jobs_full["country"] == country_name]
-    jobs_country_json = jobs_country.to_json(orient='records')
+    #jobs_country = jobs_full[jobs_full["country"] == country_name]
+    #jobs_country_json = jobs_country.to_json(orient='records')
 
-    return jobs_country_json
+    result = es.search(
+        index='bigdata-jobs',
+        query={
+        'match': {'country': country_name}
+         }
+    )
+
+    final_result = result['hits']['hits']
+
+    #return jobs_country_json
+    return final_result
+
+#http://127.0.0.1:2222/jobs/country?country=India
 
 
 @api.route("/jobs/title")
 @validate()
 def title(query:Jobs):
     job_title_name = query.title
-    jobs_title = jobs_full[jobs_full["job_title"] == job_title_name]
-    jobs_title_json = jobs_title.to_json(orient='records')
+    #jobs_title = jobs_full[jobs_full["job_title"] == job_title_name]
+    #jobs_title_json = jobs_title.to_json(orient='records')
 
-    return jobs_title_json
+    result = es.search(
+        index='bigdata-jobs',
+        query={
+        'match': {'job_title': job_title_name}
+         }
+    )
+
+    final_result = result['hits']['hits']
 
 
+    #return jobs_title_json
+    return final_result
+
+#http://127.0.0.1:2222/jobs/title?title=photographer
 
 
 
 if __name__ == "__main__":
     api.run(host="0.0.0.0",port="2222",debug=True)
+
+
+
+##curl -XGET 'localhost:9200/bigdata-jobs/doc/_validate?q=field:value’
--- flask/main.py
+++ flask/main.py
@@ -21,6 +21,20 @@ jobs_full = jobs[['job_title', 'contract_time', 'company_name', 'salary_min', 's
 jobs_full_json = jobs_full.to_json(orient='records')
 
 
+from elasticsearch import Elasticsearch
+
+
+es = Elasticsearch(
+    [{'host': 'localhost', 'port': 9200, 'scheme': 'https'}],
+    http_auth=('elastic', 'datascientest'), 
+    timeout=600,
+    verify_certs=True,  # Check SSL certificats
+    ca_certs='../elasticsearch/ca/ca.crt'  # Specify CA certificat path
+
+)
+
+
+
 @app.get('/')
 def get_index():
     return {
@@ -79,31 +93,62 @@ def jobs_tittle(jobTitle: str):
     Raises:
     Aucune exception n'est levée.
     """
-    jobs_title = jobs_full[jobs_full["job_title"] == jobTitle]
-    jobs_title_json = jobs_title.to_json(orient='records')
+    
+    #jobs_title = jobs_full[jobs_full["job_title"] == jobTitle]
+    #jobs_title_json = jobs_title.to_json(orient='records')
 
-    return jobs_title_json
+    result = es.search(
+        index='bigdata-jobs',
+        query={
+        'match': {'job_title': jobTitle}
+         }
+    )
+
+    final_result = result['hits']['hits']
+
+    #return jobs_title_json
+    return final_result
 
 #curl -X GET -i http://127.0.0.1:2222/jobs/Photographer
 
 
 @app.get("/jobs/country/{jobCountry}")
 def jobs_country(jobCountry: str):
-    jobs_country = jobs_full[jobs_full["country"] == jobCountry]
-    jobs_country_json = jobs_country.to_json(orient='records')
+    #jobs_country = jobs_full[jobs_full["country"] == jobCountry]
+    #jobs_country_json = jobs_country.to_json(orient='records')
 
-    return jobs_country_json
+    result = es.search(
+        index='bigdata-jobs',
+        query={
+        'match': {'country': jobCountry}
+         }
+    )
+
+    final_result = result['hits']['hits']
+
+    #return jobs_country_json
+    return final_result
 
 #curl -X GET -i http://127.0.0.1:2222/jobs/country/France
 
 
 @app.get("/jobs/industry/{jobIndustry}")
 def jobs_country(jobIndustry: str):
-    jobs_country = jobs_full[jobs_full["industry"] == jobIndustry]
-    jobs_country_json = jobs_country.to_json(orient='records')
+    #jobs_country = jobs_full[jobs_full["industry"] == jobIndustry]
+    #jobs_country_json = jobs_country.to_json(orient='records')
 
-    return jobs_country_json
+    result = es.search(
+        index='bigdata-jobs',
+        query={
+        'match': {'industry': jobIndustry}
+         }
+    )
 
+    final_result = result['hits']['hits']
+
+    #return jobs_country_json
+    return final_result
+     
 
 
 
