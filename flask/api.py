ca50f5aefe28fcc61a680bd19295f9d89369fe11
AMINATA NDAO <kaminandao@gmail.com>
2024-11-29 23:12:25 +0000

    update flask and fast file

--- flask/api.py
+++ flask/api.py
@@ -13,11 +13,11 @@ import pprint
 api = Flask(__name__)
 
 
-jobs = pd.read_pickle('../outputs/final/jobs.pkl')
+#jobs = pd.read_pickle('../outputs/final/jobs.pkl')
 
-jobs_full = jobs[['job_title', 'contract_time', 'company_name', 'salary_min', 'salary_max', 'publication_datetime', 'location', 'industry', 'offer_link', 'source', 'levels', 'category', 'contents', 'publication_time', 'publication_date', 'contract_type', 'country', 'state', 'city', 'postcode', 'latitude', 'longitude', 'currency_name', 'job_description', 'type_contract', 'addressCountry']]
+#jobs_full = jobs[['job_title', 'contract_time', 'company_name', 'salary_min', 'salary_max', 'publication_datetime', 'location', 'industry', 'offer_link', 'source', 'levels', 'category', 'contents', 'publication_time', 'publication_date', 'contract_type', 'country', 'state', 'city', 'postcode', 'latitude', 'longitude', 'currency_name', 'job_description', 'type_contract', 'addressCountry']]
 
-jobs_full_json = jobs_full.to_json(orient='records')
+#jobs_full_json = jobs_full.to_json(orient='records')
 
 #pprint.pprint(jobs_json, compact=True)
 
@@ -51,6 +51,81 @@ class Jobs(BaseModel):
     title:str
     #company:str
 
+@api.route("/jobs",methods=["POST","GET"])
+def jobs():
+    result = es.search(
+        size=1000,
+        index='bigdata-jobs',
+        query={
+        'match_all': {}
+         }
+    )
+
+    final_result = result['hits']['hits']
+    return final_result
+
+#exemple url
+#http://127.0.0.1:2222/jobs
+
+
+@api.route("/jobs/country")
+@validate()
+def country(query:Country):
+    country_name = query.country
+    #jobs_country = jobs_full[jobs_full["country"] == country_name]
+    #jobs_country_json = jobs_country.to_json(orient='records')
+
+    result = es.search(
+        index='bigdata-jobs',
+        query={
+        'match': {'country': country_name}
+         }
+    )
+
+    final_result = result['hits']['hits']
+
+    #return jobs_country_json
+    return final_result
+
+#exemple url
+#http://127.0.0.1:2222/jobs/country?country=India
+
+
+@api.route("/jobs/title")
+@validate()
+def title(query:Jobs):
+    job_title_name = query.title
+    #jobs_title = jobs_full[jobs_full["job_title"] == job_title_name]
+    #jobs_title_json = jobs_title.to_json(orient='records')
+
+    result = es.search(
+        index='bigdata-jobs',
+        query={
+        'match': {'job_title': job_title_name}
+         }
+    )
+
+    final_result = result['hits']['hits']
+
+
+    #return jobs_title_json
+    return final_result
+
+#exemple url
+#http://127.0.0.1:2222/jobs/title?title=photographer
+
+
+
+if __name__ == "__main__":
+    api.run(host="0.0.0.0",port="2222",debug=True)
+
+
+#exemple requete curl
+##curl -XGET 'localhost:9200/bigdata-jobs/doc/_validate?q=field:value’
+
+
+
+"""
 @api.route('/status', methods=['GET'])
 def status():
     try:
@@ -65,7 +140,7 @@ def welcome(name):
     return "Hello {} \n. Welcome to our API.".format(name)
 
 
-"""
+
 @app.route("/jobs",methods=["POST","GET"])
 def jobs():
     if request.method=="POST" :
@@ -90,75 +165,4 @@ def jobs():
     if request.method=="POST" :
         data = request.get_json()
         return "Hello {} \n".format(data["name"])
-    return jobs_full_json
-
-    """
-
-@api.route("/jobs",methods=["POST","GET"])
-def jobs():
-    result = es.search(
-        size=1000,
-        index='bigdata-jobs',
-        query={
-        'match_all': {}
-         }
-    )
-
-    final_result = result['hits']['hits']
-    return final_result
-
-#http://127.0.0.1:2222/jobs
-
-
-@api.route("/jobs/country")
-@validate()
-def country(query:Country):
-    country_name = query.country
-    #jobs_country = jobs_full[jobs_full["country"] == country_name]
-    #jobs_country_json = jobs_country.to_json(orient='records')
-
-    result = es.search(
-        index='bigdata-jobs',
-        query={
-        'match': {'country': country_name}
-         }
-    )
-
-    final_result = result['hits']['hits']
-
-    #return jobs_country_json
-    return final_result
-
-#http://127.0.0.1:2222/jobs/country?country=India
-
-
-@api.route("/jobs/title")
-@validate()
-def title(query:Jobs):
-    job_title_name = query.title
-    #jobs_title = jobs_full[jobs_full["job_title"] == job_title_name]
-    #jobs_title_json = jobs_title.to_json(orient='records')
-
-    result = es.search(
-        index='bigdata-jobs',
-        query={
-        'match': {'job_title': job_title_name}
-         }
-    )
-
-    final_result = result['hits']['hits']
-
-
-    #return jobs_title_json
-    return final_result
-
-#http://127.0.0.1:2222/jobs/title?title=photographer
-
-
-
-if __name__ == "__main__":
-    api.run(host="0.0.0.0",port="2222",debug=True)
-
-
-
-##curl -XGET 'localhost:9200/bigdata-jobs/doc/_validate?q=field:value’
+    return jobs_full_json  """
--- flask/main.py
+++ flask/main.py
@@ -14,11 +14,11 @@ import uvicorn
 app = FastAPI()
 
 
-jobs = pd.read_pickle('../outputs/final/jobs.pkl')
+#jobs = pd.read_pickle('../outputs/final/jobs.pkl')
 
-jobs_full = jobs[['job_title', 'contract_time', 'company_name', 'salary_min', 'salary_max', 'publication_datetime', 'location', 'industry', 'offer_link', 'source', 'levels', 'category', 'contents', 'publication_time', 'publication_date', 'contract_type', 'country', 'state', 'city', 'postcode', 'latitude', 'longitude', 'currency_name', 'job_description', 'type_contract', 'addressCountry']]
+#jobs_full = jobs[['job_title', 'contract_time', 'company_name', 'salary_min', 'salary_max', 'publication_datetime', 'location', 'industry', 'offer_link', 'source', 'levels', 'category', 'contents', 'publication_time', 'publication_date', 'contract_type', 'country', 'state', 'city', 'postcode', 'latitude', 'longitude', 'currency_name', 'job_description', 'type_contract', 'addressCountry']]
 
-jobs_full_json = jobs_full.to_json(orient='records')
+#jobs_full_json = jobs_full.to_json(orient='records')
 
 
 from elasticsearch import Elasticsearch
@@ -34,7 +34,92 @@ es = Elasticsearch(
 )
 
 
+@app.get("/jobs/{jobTitle}")
+def jobs_tittle(jobTitle: str):
+    """
+    Description:
+    Cette route renvoie les offres d'emploi avec le job_title indiqué en argument.
 
+    Args:
+    le titre du job recherché.
+
+    Returns:
+    - json: {\"job_title\":\"Photographer\",\"contract_time\":\"Full-time\",\"company_name\":\"CoCreativ\",\"salary_min\":\"\",\"salary_max\":\"\",\"publication_datetime\":\"2024-11-09\",\"location\":\"New York, NY\",\"industry\":\"Software Development\",\"offer_link\":\"https:\\/\\/www.linkedin.com\\/jobs\\/view\\/photographer-at-cocreativ-4072854058?position=3&pageNum=0&refId=fX7x8eYibgbCmrDQUCU9cA%3D%3D&trackingId=1U6jgNN4heUk12cCYW%2FgNA%3D%3D\",\"source\":\"linkedin\",\"levels\":\"Mid-Senior level\",\"category\":\"Other\",\"contents\":\"not available\",\"publication_time\":null,\"publication_date\":\"2024-11-09\",\"contract_type\":\"not available\",\"country\":\"United States\",\"state\":\"New York\",\"city\":\"City of New York\",\"postcode\":\"10000\",\"latitude\":\"40.7127281\",\"longitude\":\"-74.0060152\",\"currency_name\":\"US Dollar\",\"job_description\":null,\"type_contract\":null,\"addressCountry\":null}.
+
+    Raises:
+    Aucune exception n'est levée.
+    """
+    
+    #jobs_title = jobs_full[jobs_full["job_title"] == jobTitle]
+    #jobs_title_json = jobs_title.to_json(orient='records')
+
+    result = es.search(
+        index='bigdata-jobs',
+        query={
+        'match': {'job_title': jobTitle}
+         }
+    )
+
+    final_result = result['hits']['hits']
+
+    #return jobs_title_json
+    return final_result
+
+#Exemple requete curl
+# curl -X GET -i http://127.0.0.1:2222/jobs/Photographer
+
+
+@app.get("/jobs/country/{jobCountry}")
+def jobs_country(jobCountry: str):
+    #jobs_country = jobs_full[jobs_full["country"] == jobCountry]
+    #jobs_country_json = jobs_country.to_json(orient='records')
+
+    result = es.search(
+        index='bigdata-jobs',
+        query={
+        'match': {'country': jobCountry}
+         }
+    )
+
+    final_result = result['hits']['hits']
+
+    #return jobs_country_json
+    return final_result
+
+#Exemple requete curl
+#curl -X GET -i http://127.0.0.1:2222/jobs/country/France
+
+
+@app.get("/jobs/industry/{jobIndustry}")
+def jobs_country(jobIndustry: str):
+    #jobs_country = jobs_full[jobs_full["industry"] == jobIndustry]
+    #jobs_country_json = jobs_country.to_json(orient='records')
+
+    result = es.search(
+        index='bigdata-jobs',
+        query={
+        'match': {'industry': jobIndustry}
+         }
+    )
+
+    final_result = result['hits']['hits']
+
+    #return jobs_country_json
+    return final_result
+     
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=2222)
+
+#uvicorn main:app --reload --port 2222
+
+#http://127.0.0.1:2222/doc
+#http://127.0.0.1:2222/redoc
+
+
+"""
 @app.get('/')
 def get_index():
     return {
@@ -76,86 +161,4 @@ def patch_index():
         'method': 'patch',
         'endpoint': '/'
         }
-
-
-@app.get("/jobs/{jobTitle}")
-def jobs_tittle(jobTitle: str):
-    """
-    Description:
-    Cette route renvoie les offres d'emploi avec le job_title indiqué en argument.
-
-    Args:
-    le titre du job recherché.
-
-    Returns:
-    - json: {\"job_title\":\"Photographer\",\"contract_time\":\"Full-time\",\"company_name\":\"CoCreativ\",\"salary_min\":\"\",\"salary_max\":\"\",\"publication_datetime\":\"2024-11-09\",\"location\":\"New York, NY\",\"industry\":\"Software Development\",\"offer_link\":\"https:\\/\\/www.linkedin.com\\/jobs\\/view\\/photographer-at-cocreativ-4072854058?position=3&pageNum=0&refId=fX7x8eYibgbCmrDQUCU9cA%3D%3D&trackingId=1U6jgNN4heUk12cCYW%2FgNA%3D%3D\",\"source\":\"linkedin\",\"levels\":\"Mid-Senior level\",\"category\":\"Other\",\"contents\":\"not available\",\"publication_time\":null,\"publication_date\":\"2024-11-09\",\"contract_type\":\"not available\",\"country\":\"United States\",\"state\":\"New York\",\"city\":\"City of New York\",\"postcode\":\"10000\",\"latitude\":\"40.7127281\",\"longitude\":\"-74.0060152\",\"currency_name\":\"US Dollar\",\"job_description\":null,\"type_contract\":null,\"addressCountry\":null}.
-
-    Raises:
-    Aucune exception n'est levée.
-    """
-    
-    #jobs_title = jobs_full[jobs_full["job_title"] == jobTitle]
-    #jobs_title_json = jobs_title.to_json(orient='records')
-
-    result = es.search(
-        index='bigdata-jobs',
-        query={
-        'match': {'job_title': jobTitle}
-         }
-    )
-
-    final_result = result['hits']['hits']
-
-    #return jobs_title_json
-    return final_result
-
-#curl -X GET -i http://127.0.0.1:2222/jobs/Photographer
-
-
-@app.get("/jobs/country/{jobCountry}")
-def jobs_country(jobCountry: str):
-    #jobs_country = jobs_full[jobs_full["country"] == jobCountry]
-    #jobs_country_json = jobs_country.to_json(orient='records')
-
-    result = es.search(
-        index='bigdata-jobs',
-        query={
-        'match': {'country': jobCountry}
-         }
-    )
-
-    final_result = result['hits']['hits']
-
-    #return jobs_country_json
-    return final_result
-
-#curl -X GET -i http://127.0.0.1:2222/jobs/country/France
-
-
-@app.get("/jobs/industry/{jobIndustry}")
-def jobs_country(jobIndustry: str):
-    #jobs_country = jobs_full[jobs_full["industry"] == jobIndustry]
-    #jobs_country_json = jobs_country.to_json(orient='records')
-
-    result = es.search(
-        index='bigdata-jobs',
-        query={
-        'match': {'industry': jobIndustry}
-         }
-    )
-
-    final_result = result['hits']['hits']
-
-    #return jobs_country_json
-    return final_result
-     
-
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=2222)
-
-#uvicorn main:app --reload --port 2222
-
-#http://127.0.0.1:2222/doc
-#http://127.0.0.1:2222/redoc
+"""
